4e90ea0d6438547b09e0514370b159a03177a50b
kimseeun <cocobell3@naver.com>
2024-02-02 01:36:56 +0900

    feat: 로그인 기능 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { AuthSignInDto } from './dto/auth-signin.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -20,5 +21,15 @@ export class UsersController {
   ): Promise<AuthCredentialsDto> {
     await this.usersService.signUp(authCredentialsDto);
     return authCredentialsDto;
+  }
+
+  @Post('signin')
+  @UsePipes(ValidationPipe)
+  async signIn(
+    @Body() authSignInDto: AuthSignInDto,
+  ): Promise<{ accessToken: string; userInfo: object }> {
+    const result = await this.usersService.signIn(authSignInDto);
+    console.log('signin_controller::', result);
+    return result;
   }
 }
