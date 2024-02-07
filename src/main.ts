7f4c707530d813a055c006c247f17395a42aefcc
FonDitbul <niki7084@naver.com>
2024-02-07 14:08:11 +0900

    feat : setting response and class validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ResponseTransformInterceptor } from './common/response.transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ResponseTransformInterceptor());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
